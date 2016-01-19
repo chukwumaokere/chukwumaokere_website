b2128b0cc54692d5c6a905c54977a47b62653283
Chukwuma Okere <chukwumaokere@Yahoo.com>
2016-01-19 05:13:03 -0600

    updated contact forms

--- contactme.php
+++ contactme.php
@@ -1,4 +1,25 @@
 <?php
+/*
+include  'contactme.html';
+$name=$_POST['name'];
+$email_from = $_POST['email'];
+$comment=$_POST['comment'];
 
+$email_to = "chuwkumamokere@yahoo.com";
 
+$email_subject = "Contact from chukwumaokere.com";
+
+$email_message = "Form details below.\n\n";
+$email_message .= "First Name: ".clean_string($name)."\n";
+$email_message .= "Email: ".clean_string($email_from)."\n";
+$email_message .= "Comment: ".clean_string($comment)."\n";
+
+$headers = 'From: '.$email_from."\r\n".
+
+		'Reply-To: '.$email_from."\r\n" .
+
+		'X-Mailer: PHP/' . phpversion();
+
+mail($email_to, $email_subject, $email_message, $headers);
+*/
 header("location:./thankyoucontact.html");
